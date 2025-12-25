5cddb31f161cdbc0d8c50b6004140424a1726627
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2025-12-25 11:22:00 +0500

    Refactor Hero section for improved layout, responsiveness, and visual appeal

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,17 +1,18 @@
 const Hero = () => {
   return (
-    <section className="flex justify-between mr-35 ml-20" id="hero">
-        <div className="text-sm leading-10  mt-25">
-        <h1 className="text-2xl mb-6 mt-6 font-semibold">Hi I'm Jawairia Mazhar</h1>
-        <p className="indent-4 leading-7 text-wrap max-w-4xl text-base tracking-wide">
+    <section className="flex flex-col md:flex-row justify-between items-center px-6 md:px-20 py-10" id="hero">
+        <img className="w-70 h-70 mt-5 md:w-120 md:h-120 mx-auto object-contain md:mx-0 mb-8 md:mb-0" src="\src\assets\hijab_logo.png" alt="Cover image" />
+
+        <div className="text-xs md:text-sm md:leading-10  md:mt-25">
+          <h1 className="text-xl md:text-5xl mb-4 font-semibold">Hi I'm Jawairia Mazhar</h1>
+          <p className="p-1 indent-4 md:leading-7 text-wrap md:max-w-4xl text-base tracking-wide">
           A Computer Science student and aspiring web developer. I love creating
           interactive web experiences using JavaScript, React, and Tailwind CSS.
           I learn by building projects, exploring UI/UX, and constantly
           improving my skills. My goal is to craft websites that are not just
           functional, but also engaging and user-friendly.
-        </p> 
+          </p> 
         </div>
-        <img className="w-120 h-120 mt-15" src="\src\assets\hijab_logo.png" alt="Cover image" />
       </section>
   );
 }
