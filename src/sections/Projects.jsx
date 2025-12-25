5eaa6797a873d448c0b908f654c51bbd37c79ef3
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2025-12-25 23:18:54 +0500

    Refactor Projects section for improved layout and code clarity

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -3,23 +3,19 @@ import ProjectCard from '../components/ProjectCard'
 
 const Projects = () => {
   return (
-    <section className="mt-8 mb-10 md:mb-40 " id="projects">
+    <section className="mt-8 mb-10 md:scroll-mt-24 md:mb-40 " id="projects">
       <h1 className="text-2xl md:text-4xl font-medium text-center mb-4 md:mb-12">My Work</h1>
       <div className='grid gap-y-6 md:grid-cols-3 place-items-center md:gap-y-20 md:place-items-center'>
-            {/* <div 
-            className="w-110 h-90 bg-cover border border-red-500 rounded-md shadow-xl hover:shadow-2xl transition duration-300" 
-            style={{ backgroundImage: "url('src/assets/P1CoffeeWeb.png')" }}> */}
-            {/* <div className="w-110 h-75 mt bg-cover"
-            style={{ backgroundImage: "url('src/assets/P1CoffeeWeb.png')" }}> */}
-                <ProjectCard
-                    title = "Coffee Shop Website" 
-                    image="src\assets\P1CoffeeWeb.png" alt="Coffee Shop Website"
-                    description="A modern, responsive landing page designed to showcase a café's menu and ambiance through an interactive user experience. It features smooth scrolling, a dynamic search overlay, and a mobile-optimized menu."
-                    role="UI/UX Design & Frontend Development."
-                    tech="HTML5, CSS3 (Custom Variables), JavaScript (ES6+)"
-                    outcome="Successfully delivered a high-performance site with optimized CTA (Call-to-Action) buttons and seamless mobile navigation."
-                    linkToWeb="https://jawairia-mazhar.github.io/Responsive-Coffee-Shop-Website/"
-                />
+          
+           <ProjectCard
+             title = "Coffee Shop Website" 
+             image="src\assets\P1CoffeeWeb.png" alt="Coffee Shop Website"
+             description="A modern, responsive landing page designed to showcase a café's menu and ambiance through an interactive user experience. It features smooth scrolling, a dynamic search overlay, and a mobile-optimized menu."
+             role="UI/UX Design & Frontend Development."
+             tech="HTML5, CSS3 (Custom Variables), JavaScript (ES6+)"
+             outcome="Successfully delivered a high-performance site with optimized CTA (Call-to-Action) buttons and seamless mobile navigation."
+             linkToWeb="https://jawairia-mazhar.github.io/Responsive-Coffee-Shop-Website"
+          />
 
             {/* </div> */}
             <ProjectCard 
@@ -30,8 +26,8 @@ const Projects = () => {
             tech="HTML, CSS (Flexbox/Grid), JavaScript (ScrollReveal)"
             outcome="Mastered advance CSS positioning and practiced grid systems and dynamic content rendering."
             linkToWeb="https://endearing-pony-af6261.netlify.app/"
-
             />
+
             <ProjectCard 
             title = "Foodie Zone Web" 
             image="src\assets\P3Food.png"
