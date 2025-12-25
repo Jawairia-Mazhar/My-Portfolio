b389d3025c098e5a4491987869bc4332da1715d2
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2025-12-25 11:21:15 +0500

    Refactor Navbar component for improved responsiveness and layout consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import { HashLink } from 'react-router-hash-link';
 
 export default function Navbar() {
   return (
-    <nav className="flex p-4 justify-between  shadow-md bg-white sticky top-0 z-50 items-center">
+    <nav className="flex md:p-4 md:justify-between justify-between shadow-md bg-white sticky top-0 z-50 items-center w-full">
       <img src="src/assets/JMIcon.png" alt="logo" className="w-12 h-12 items-start" />
-      <ul className="flex gap-6 mr-148">
+      <ul className="flex md:gap-6 gap-4 md:mr-148 ">
         <HashLink smooth to="/#hero" className='hover:text-teal-400 hover:font-bold'>Home</HashLink>
         <HashLink smooth to="/skills#skills" className='hover:text-teal-400 hover:font-bold'>Skills</HashLink>
         <HashLink smooth to="/projects#projects" className='hover:text-teal-400 hover:font-bold'>Projects</HashLink>
