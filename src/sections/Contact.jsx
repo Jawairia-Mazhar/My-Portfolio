cf1ec557a8510faa871bdebe1ebd21118a112b1e
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2025-12-25 12:01:14 +0500

    Refactor Contact section for improved layout and responsiveness

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -3,22 +3,22 @@ import React from 'react'
 const Contact = () => {
   return (
     <section id="contact">
-        <h1 className="text-4xl font-bold text-center mb-12">Contact Me</h1>
+        <h1 className="text-2xl mb-8 md:text-4xl font-bold text-center md:mb-12">Contact Me</h1>
         <div className='grid grid-cols-3 place-items-center'>
         {/* Gmail */}
-        <div className="flex gap-4 mb-6">
+        <div className="flex gap-2 md:gap-4 mb-6">
           <img src="src\assets\gmail.png" alt="gmail"
-          className="w-10 h-10"/>
+          className="w-8 h-8 md:w-10 md:h-10"/>
           <a href="mailto:Jawairiamazhar21@gmail.com" target="_blank" className="p-2 hover:font-bold hover:text-red-600">Gmail</a>
         </div>
         {/* LinkedIn */}
-        <div className="flex gap-4 mb-6">
-          <img src="src\assets\linkedin.png" alt="linkedin" className="w-10 h-10" />
+        <div className="flex gap-2 md:gap-4 mb-6">
+          <img src="src\assets\linkedin.png" alt="linkedin" className="w-8 h-8 md:w-10 md:h-10" />
           <a href="https://www.linkedin.com/in/jawairiamazhar21/" target="_blank" className="p-2 hover:font-bold hover:text-blue-600">LinkedIn</a>
         </div>
         {/* Github */}
-        <div className="flex gap-4 mb-6">
-          <img src="src\assets\githubicon.png" alt="github" className="w-10 h-10" />
+        <div className="flex gap-2 md:gap-4 mb-6">
+          <img src="src\assets\githubicon.png" alt="github" className="w-8 h-8 md:w-10 md:h-10" />
           <a href="https://github.com/Jawairia-Mazhar" target="_blank" className="p-2 hover:font-bold">Github</a>
         </div>
         </div>
