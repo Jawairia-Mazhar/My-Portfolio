74cbd777a619319a44af3334c6f290f478a1ea97
Jawairia <145025420+Jawairia-Mazhar@users.noreply.github.com>
2026-02-06 12:33:19 +0500

    Refactor Navbar component to wrap logo in HashLink for improved navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,15 @@ import logo from '../assets/JMIcon.png';
 export default function Navbar() {
   return (
     <nav className="flex p-1 md:p-4 md:justify-between justify-between shadow-md bg-white sticky top-0 z-50 items-center w-full">
-      <img src={logo} alt="logo" className="w-12 h-12 items-start" />
+      <HashLink smooth to="#hero" className="mr-4">
+        <img src={logo} alt="logo" className="w-12 h-12 items-start" />
+      </HashLink>
 
-      <ul className="flex mr-5 md:gap-6 gap-4 md:mr-148 ">
-        <HashLink smooth to="/#hero" className='hover:text-teal-400 hover:font-bold'>Home</HashLink>
-        <HashLink smooth to="/skills#skills" className='hover:text-teal-400 hover:font-bold'>Skills</HashLink>
-        <HashLink smooth to="/projects#projects" className='hover:text-teal-400 hover:font-bold'>Projects</HashLink>
-        <HashLink smooth to="/contact#contact" className='hover:text-teal-400 hover:font-bold'>Contact</HashLink>
+      <ul className="flex mr-5 md:gap-12 gap-4 md:mr-140 ">
+        <HashLink smooth to="#hero" className="navlink hover:font-bold hover:text-teal-300">Home</HashLink>
+        <HashLink smooth to="#skills" className="navlink hover:text-teal-300 hover:font-bold">Skills</HashLink>
+        <HashLink smooth to="#projects" className="navlink hover:font-bold hover:text-teal-300">Projects</HashLink>
+        <HashLink smooth to="#contact" className="navlink hover:font-bold hover:text-teal-300">Contact</HashLink>
       </ul>
     </nav>
   );
